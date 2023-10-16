1a906cddee9372b35237916edd79c86cf2b7abd4
Pieterjan De Clippel <pieterjandeclippel@msn.com>
2023-10-16 22:25:28 +0200

    Store background color in statekey to prevent resending a webrequest during csr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
+import { Component, Inject, PLATFORM_ID, OnInit } from '@angular/core';
+import { TransferState, makeStateKey } from '@angular/platform-browser';
+
+const backgroundColorState = makeStateKey<string>('backgroundColor');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(@Inject(PLATFORM_ID) private platformId: any, private transferState: TransferState) {}
+
   title = 'TransferStateDemo';
+  backgroundColor?: string;
+
+  ngOnInit() {
+    if (isPlatformServer(this.platformId)) {
+      // We should be able to receive the background color from a SupplyData hook...
+      this.backgroundColor = '#555';
+      this.transferState.set(backgroundColorState, this.backgroundColor);
+    } else {
+      const colorFromState = this.transferState.get(backgroundColorState, undefined);
+      if (colorFromState) {
+        this.backgroundColor = colorFromState;
+      } else {
+        // This can happen when we're on a page instead of a rootcomponent,
+        // and the value is to be fetched by the browser when the user navigates to this page.
+      }
+    }
+  }
 }
